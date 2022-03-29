9a2c59d00d4c0cc5fb29c178f744776e9262ba33
minahomi <102497664+minahomi@users.noreply.github.com>
2022-03-29 15:04:09 +0900

    Update images.js

--- images.js
+++ images.js
@@ -1,11 +1,55 @@
+//handle-DarkGoldenrod
 function showimg1(){
-document.area1.src = "https://static.wixstatic.com/media/a7f31f_e0ce83979ff9433cb2b5dfe71512140c~mv2.png";
-}
-
+    document.area1.src = "https://static.wixstatic.com/media/a7f31f_e0ce83979ff9433cb2b5dfe71512140c~mv2.png";
+    }
+ //handle-Peru   
 function showimg2(){
-document.area1.src = "https://static.wixstatic.com/media/a7f31f_b061fa08df164f0f95a880ad550e412c~mv2.png";
-}
+    document.area1.src = "https://static.wixstatic.com/media/a7f31f_b061fa08df164f0f95a880ad550e412c~mv2.png";
+    }
 
+//handle-DarkRed  
 function showimg3(){
-document.area1.src = "https://static.wixstatic.com/media/a7f31f_09a635ca8e864318a0b8e7f9c7421e48~mv2.png";
-}
+    document.area1.src = "https://static.wixstatic.com/media/a7f31f_09a635ca8e864318a0b8e7f9c7421e48~mv2.png";
+    }
+
+//handle-SaddleBrown
+function showimg4(){
+    document.area2.src = "https://static.wixstatic.com/media/a7f31f_a9611e15dc4c4b2f8faa5836661f8939~mv2.png";
+    }
+//pattern-PaleVioletred
+function showimg5(){
+    document.area2.src = "https://static.wixstatic.com/media/a7f31f_199e476811724674ad8f08c7780734d2~mv2.png";
+    }
+//pattern-DarkSalmon
+function showimg6(){
+    document.area2.src = "https://static.wixstatic.com/media/a7f31f_4c4ce6e8b84c4f2999b306f92c2c006a~mv2.png";
+    }
+
+//pattern-Goldenrod
+function showimg7(){
+    document.area2.src = "https://static.wixstatic.com/media/a7f31f_c88806522f9e4536ba72ea80f7f64e7f~mv2.png";
+    }
+
+//pattern-Khaki
+function showimg8(){
+    document.area2.src = "https://static.wixstatic.com/media/a7f31f_45771f77b7524c7088ae042c2aab25e1~mv2.png";
+    }
+
+//bottom-Maroon
+function showimg9(){
+    document.area3.src = "https://static.wixstatic.com/media/a7f31f_26d99b49594f455496e65deaa38b5feb~mv2.png";
+    }
+
+//bottom-SteelBlue
+function showimg10(){
+    document.area3.src = "https://static.wixstatic.com/media/a7f31f_ae1850603b834232bd54b33a3199c91f~mv2.png";
+    }
+//bottom-DarkOliveGreen
+function showimg11(){
+    document.area3.src = "https://static.wixstatic.com/media/a7f31f_018cb0991c374bafb0c655440afbde0e~mv2.png";
+    }
+
+//bottom-Sienna
+function showimg12(){
+    document.area3.src = "https://static.wixstatic.com/media/a7f31f_963476b9fbb745c0806fc131e8f42640~mv2.png";
+    }
