e05fcc872e76ec325dfe4f84e66f31559759538c
minahomi <102497664+minahomi@users.noreply.github.com>
2022-03-29 13:21:42 +0900

    Add files via upload

---
+++ gazo.js
@@ -0,0 +1,11 @@
+function showimg1(){
+document.area1.src = "https://static.wixstatic.com/media/a7f31f_e0ce83979ff9433cb2b5dfe71512140c~mv2.png";
+}
+
+function showimg2(){
+document.area1.src = "https://static.wixstatic.com/media/a7f31f_b061fa08df164f0f95a880ad550e412c~mv2.png";
+}
+
+function showimg3(){
+document.area1.src = "https://static.wixstatic.com/media/a7f31f_09a635ca8e864318a0b8e7f9c7421e48~mv2.png";
+}
